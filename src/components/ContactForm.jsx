182b4283d0192d75f304e02d895b87cdf0d7a76f
Karan <email@karanshergill.com>
2024-08-09 17:43:36 +0530

    form html

--- src/components/ContactForm.jsx
+++ src/components/ContactForm.jsx
@@ -1,7 +1,43 @@
-import React from 'react'
+import { useForm } from 'react-hook-form'
 
 export const ContactForm = () => {
-  return (
-    <div>ContactForm</div>
-  )
+
+    const { register, handleSubmit, formState: { errors }, reset } = useForm();
+
+    const onSubmit = data => {
+        console.log('Form Data:', data);
+        reset();
+    }
+
+    return (
+        <form onSubmit={handleSubmit(onSubmit)}>
+            <div className='flex'>
+                {/* First Name */}
+                <div>
+                    <label htmlFor="firstName">First Name</label>
+                    <input type="text" id="firstName"
+                        {...register('firstName', { required: 'This field is required' })}
+                        className={`border-2 ${errors.firstName ? 'border-red-600' : ''}`}
+                    />
+                    {errors.firstName && (
+                        <p>{errors.firstName.message}</p>
+                    )}
+                </div>
+
+                {/* Last Name */}
+                <div>
+                    <label htmlFor="lastName">Last Name</label>
+                    <input type="text" id="lastName"
+                        {...register('lastName', { required: 'This field is required' })}
+                        className={`border-2 ${errors.lastName ? 'border-red-600' : ''}`}
+                    />
+                    {errors.lastName && (
+                        <p>{errors.lastName.message}</p>
+                    )}
+                </div>
+            </div>
+
+            <button type="submit">Submit</button>
+        </form >
+    )
 }
