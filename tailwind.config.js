edb95d7df514614f4d833734f3bea60740a8d129
Karan <email@karanshergill.com>
2024-08-09 17:47:44 +0530

    add custom colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,7 +7,12 @@ export default {
   theme: {
     extend: {
       colors: {
-
+        "Color-Green-Light": 'hsl(148, 38%, 91%)',
+        "Color-Green-Medium": 'hsl(169, 82%, 27%)',
+        "Color-Red": 'hsl(0, 66%, 54%)',
+        "Color-White": 'hsl(0, 0%, 100%)',
+        "Color-Grey-Medium": 'hsl(186, 15%, 59%)',
+        "Color-Grey-Dark": 'hsl(187, 24%, 22%)',
       },
       fontFamily: {
         "Karla" : ["Karla", "sans-serif"]
